5934f8ea3122526726e8acdf340216b64732f3a9
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-07 15:23:03 +0530

    Add files via upload

---
+++ 5th.py
@@ -0,0 +1,3 @@
+a=float(input("enter the temperature in Celsius:"))
+b=(a*9/5)+32
+print("Temperature in Fahrenheit is:", b)
---
+++ 6th.py
@@ -0,0 +1,3 @@
+a=float(input("enter the temperature in Fahrenheit:"))
+b=(a-32)*5/9
+print("Temperature in Celsius is:", b)
